b9f7019f744e5b28c6be69a1973ea66941c4368d
Viraj motivaras <viraj.motivaras@scania.com>
2020-04-20 15:34:45 +0200

    Aded more understanding points to readme and added additional routes for the counter

--- flashcards.py
+++ flashcards.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+visitor_count = 0
+
 @app.route("/")
 def welcome():
     return "Welcome to flash cards application"
+
+@app.route("/viewed")
+def visitors_count():
+    global visitor_count
+    visitor_count += 1
+    return "This pae has been viewed {count} number of times".format(count=visitor_count)
+
