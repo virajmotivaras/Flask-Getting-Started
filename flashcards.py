988ab021d281be8aac61bd3d9ac6a893886d5da6
Viraj motivaras <viraj.motivaras@scania.com>
2020-04-20 16:22:43 +0200

    Added more understanding points to readme Passing a variable to a Jinja template

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,11 +6,6 @@ visitor_count = 0
 
 @app.route("/")
 def welcome():
-    return "Welcome to flash cards application"
-
-@app.route("/viewed")
-def visitors_count():
-    global visitor_count
-    visitor_count += 1
-    return "This pae has been viewed {count} number of times".format(count=visitor_count)
+    return render_template("welcome.html",
+                           message="This is a message from the view")
 
