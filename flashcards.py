1eef56fe8b3680a4b9c48486efdf70ac3b1e6a41
Viraj motivaras <viraj.motivaras@scania.com>
2020-04-21 11:22:22 +0200

    Added a post method Saving it to the db

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -9,6 +9,7 @@ visitor_count = 0
 def welcome():
     return render_template("welcome.html",
                            cards=db)
+
 
 @app.route("/card/<int:index>")
 def card_view(index):
@@ -21,9 +22,24 @@ def card_view(index):
     except IndexError:
         abort(404)
 
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        #do the processing here
+        card = {"question" : request.form["question"],
+                "answer": request.form["answer"]}
+        db.append(card)
+        save_db()
+        return redirect(url_for("card_view", index=len(db)-1))
+    else:
+        return render_template("add_card.html")
+
+
 @app.route("/api/card")
 def api_card_list():
     return jsonify(db)
+
 
 @app.route("/api/card/<int:index>")
 def api_card_detail(index):
--- model.py
+++ model.py
@@ -1,8 +1,14 @@
 import json
 
+
 def load_db():
     with open("flashards_db.json") as f:
         return json.load(f)
+
+
+def save_db():
+    with open("flashcards_db.json", 'w' ) as f:
+        return json.dump(db, f)
 
 
 db = load_db()
